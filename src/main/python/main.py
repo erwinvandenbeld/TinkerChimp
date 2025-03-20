67ea679ca588a368d9be554176e5191d765799f4
Vincent Heet <vincent.heet@topicus.nl>
2025-03-20 08:44:55 +0100

    Add polly

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,6 +6,12 @@ from awscrt import mqtt5, http
 import threading
 from concurrent.futures import Future
 from shaker import Shaker
+import urllib3
+import json
+import boto3
+from contextlib import closing
+import os
+from tempfile import gettempdir
 
 TIMEOUT = 20
 topic_filter = "chimp/topic"
@@ -14,7 +20,8 @@ received_all_event = threading.Event()
 future_stopped = Future()
 future_connection_success = Future()
 shaker = None
-
+cert_filepath='../../../certs/iot-certificate.pem.crt'
+pri_key_filepath='../../../certs/iot-private.pem.key'
 
 def on_publish_received(publish_packet_data):
     publish_packet = publish_packet_data.publish_packet
@@ -52,6 +59,33 @@ def read_arguments() -> dict[str, str]:
 
     return parser.parse_args()
 
+def get_boto_session():
+    print("Getting STS token")
+    resp = http.request("GET","https://<TODO>.credentials.iot.eu-west-1.amazonaws.com/role-aliases/iot-chim-ara-flapflap-role-alias/credentials")
+    print(resp.status)
+    print(resp.data)
+    if resp.status != 200:
+        raise Exception("Failed to retrieve STS token: {}".format(resp.reason))
+    global creds 
+    creds = json.loads(resp.data)['credentials']
+    # return boto3.session.Session(aws_access_key_id=creds['accessKeyId'], aws_secret_access_key=creds['secretAccessKey'], aws_session_token=creds['sessionToken'], region_name=region)
+    return boto3.Session(
+        aws_access_key_id=creds['accessKeyId'],
+        aws_secret_access_key=creds['secretAccessKey'],
+        aws_session_token=creds['sessionToken']
+        )
+
+
+retries = urllib3.Retry(connect=5, read=2, redirect=5)
+http = urllib3.PoolManager(
+    cert_file=cert_filepath,
+    key_file=pri_key_filepath,
+    cert_reqs="CERT_REQUIRED",
+    headers={
+        'x-amzn-iot-thingname': 'Flapflap'
+    },
+    retries=retries
+)
 
 if __name__ == '__main__':
 
@@ -61,12 +95,43 @@ if __name__ == '__main__':
     message_topic = arguments.topic
     shaker = Shaker(arguments.no_gpio == None)
 
+    sess = get_boto_session()
+    polly = sess.client('polly', region_name='eu-west-1')
+    response = polly.synthesize_speech(
+        OutputFormat='mp3',
+        Text='Hello, this is a test from the AWS SDK for Python',
+        TextType='text',
+        VoiceId='Brian'
+    )
+    print(response)
+
+    # Access the audio stream from the response
+    if "AudioStream" in response:
+        # Note: Closing the stream is important because the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+        with closing(response["AudioStream"]) as stream:
+                output = os.path.join(gettempdir(), "iniIoTmessage.mp3")
+                print("Writing audio stream to file {}".format(output))
+                try:
+                    # Open a file for writing the output as a binary stream
+                    with open(output, "wb") as file:
+                        file.write(stream.read())
+                        
+                except IOError as error:
+                    # Could not write to file, exit gracefully
+                    print(error)
+                    sys.exit(-1)
+
+
+    print("I got here")
 
     client = mqtt5_client_builder.mtls_from_path(
         endpoint=arguments.endpoint,
         port=8883,
-        cert_filepath='../../../certs/iot-certificate.pem.crt',
-        pri_key_filepath='../../../certs/iot-private.pem.key',
+        cert_filepath=cert_filepath,
+        pri_key_filepath=pri_key_filepath,
         on_publish_received=on_publish_received,
         on_lifecycle_stopped=on_lifecycle_stopped,
         on_lifecycle_connection_success=on_lifecycle_connection_success,
